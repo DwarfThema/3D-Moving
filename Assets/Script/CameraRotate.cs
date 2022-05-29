165afffb2fa098368866c4cd70e6d6f3542068f9
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-05-29 22:08:45 +0900

    ScreenPointToRay

--- Assets/Script/CameraRotate.cs
+++ Assets/Script/CameraRotate.cs
@@ -7,23 +7,35 @@ public class CameraRotate : MonoBehaviour
     float rotateX;
     float rotateY;
 
-    public float rotateSpeed = 500;
+    public float rotateSpeed = 100;
 
     Vector3 cameraNear;
     Vector3 cameraFar;
 
+    float wheelValue = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        cameraNear = transform.position;
-
-        cameraFar = transform.forward * -5;
+        cameraNear = transform.localPosition;
+        //localPosition 으로 내 부모를 기준으로한 위치로 간다.
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        cameraFar = cameraNear + transform.forward * -5;
+
+        wheelValue -= Input.GetAxis("Mouse ScrollWheel");
+        wheelValue = Mathf.Clamp(wheelValue, 0, 1.0f);
+
+        Vector3 cameraPosition = Vector3.Lerp(cameraNear, cameraFar, wheelValue);
+
+        transform.localPosition = cameraPosition;
+
+
         float axisX = Input.GetAxis("Mouse X");
         float axisY = Input.GetAxis("Mouse Y");
 
@@ -33,6 +45,5 @@ public class CameraRotate : MonoBehaviour
         rotateX = Mathf.Clamp(rotateX, -80, 80);
 
         transform.eulerAngles = new Vector3(-rotateX, rotateY, 0);
-
     }
 }
---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray currentRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(currentRay, out hitInfo))
+        {
+            print(hitInfo.transform.name);
+        }
+        else
+        {
+            print("아무것도 없다.");
+        }
+    }
+
+}
