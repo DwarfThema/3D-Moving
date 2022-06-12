e627e35561a7f3c0e797528e55cf1837dcae453d
DwarfThema <86091261+DwarfThema@users.noreply.github.com>
2022-06-12 23:47:36 +0900

    mea fin

---
+++ Assets/CoTee/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.name);
+            }
+
+        }
+    }
+}
